f2f6e971f3a38772190e6838ea046156c586bc17
Pranav Dhawan <dhawan.pranav8@gmail.com>
2025-03-18 23:31:23 +0530

    deploy fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import './App.css'
 import Home from './x'
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import SecondCal from './y';
 
 const App: React.FC = () => {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Navigate to="/first" replace />} />
           <Route path='/first' element={<SecondCal />} />
           <Route path='/second' element={<Home />} />
         </Routes>
       </BrowserRouter>
-
     </>
   )
 }
